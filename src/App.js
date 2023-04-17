9df11cfacaa169bf0331577d53c19e2f454b163a
Jose Jaime Come <josejaimecome@gmail.com>
2023-04-17 21:19:23 +0200

    Code for populating data from server added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 import Button from './components/Button';
+import axios from 'axios'
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   const divStyle = {
     with: '800px',
   };
@@ -26,15 +28,29 @@ function App() {
   const alltasks = {
 
   };
-  const addTask = () => {
-
-  };
-  const getData = () => {
+  const addTask = async () => {
     return ''
+  };
+  const getData = async () => {
+    const res = await axios.get("http://127.0.0.1:3000/task");
+    setTasks(res.data)
+    console.log(res);
   };
   const TasksList = (v) => {
     return ''
-  }
+  };
+  const completed = (v) => {
+    return ''
+  };
+  const taskStyle = {
+    padding: '8px',
+    width: '80%',
+    border: '1px solid gray',
+  };
+  const statusStyle = {
+    fontSize: '24px',
+    color: '#EC7063',
+  };
   return (
     <div style={ divStyle }>
       <h1 style={ centerText }>Reactjs with Redux<br />(Task management)</h1><br />
@@ -45,7 +61,7 @@ function App() {
           style={{ width: '180px' }} />
         <Button
           text={'Add Task'}
-          onClick={ addTask }
+          onClick={ () => addTask() }
           class_name={'btn btn-primary'}
         />
       </div>
@@ -54,25 +70,49 @@ function App() {
           <div style={ marginBottom }>
             <Button
               text={'All tasks'}
-              onClick={ getData }
+              onClick={ () => getData() }
               class_name={'btn btn-primary'}
               style={{ paddingLeft: '4px', marginRight: '4px' }}
             />
             <Button
               text={'Ongoing'}
-              onClick={ TasksList('Ongoing') }
+              onClick={ () => TasksList('Ongoing') }
               class_name={'btn btn-primary'}
               style={{ paddingLeft: '4px', marginRight: '4px' }}
             />
             <Button
               text={'Completed'}
-              onClick={ TasksList('Completed') }
+              onClick={ () => TasksList('Completed') }
               class_name={'btn btn-primary'}
               style={{ paddingLeft: '4px', marginRight: '4px' }}
             />           
           </div>
           <div style={ alltasks }>
-            
+            {
+              tasks.map((task) => ( 
+                <div key={tasks.indexOf(task)} style={ taskStyle } >
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td><div id="iddiv">{ task.id }</div></td>
+                        <td>{ task.task }</td>
+                        <td><i v-show="task.status === 'Completed'" class="bi bi-check-circle-fill" 
+                          style={ statusStyle }></i></td>
+                        <td>
+                          <Button
+                            v-show="task.status === 'Ongoing'"
+                            text={'Mark as completed'}
+                            onClick={ () => completed(task) }
+                            class_name={'btn btn-primary'}
+                            style={{ paddingLeft: '4px', marginRight: '4px' }}
+                          />    
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>      
+                </div>
+              ))
+            }
           </div>
       </div>
 
